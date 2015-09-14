6771f9bebc5ddbd6f3977dd6df9b9210265ff6df
Kyle Pratuch & Annie Hoogendoorn <student@epicodus.com>
2015-09-14 16:28:21 -0700

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(event) {
     $("#add-landmark").click(function() {
+        //Add new input field for additional places
         $("#new-landmarks").append(
                                     '<div class="form-group">' +
                                         '<label for="new-landmarks">Landmark</label>' +
@@ -12,24 +13,28 @@ $(document).ready(function(event) {
     $("form#new-place").submit(function(event) {
         event.preventDefault();
 
+        //Get values from form and use as variables
         var inputPlaceName = $("input#new-place-name").val();
         var inputLocation = $("input#new-location").val();
         var inputDate = $("input#new-date").val();
         var inputNotes = $("textarea#new-notes").val();
 
+        //Creates newPlace object (like PHP constructor)
         var newPlace = { placeName: inputPlaceName, placeLocation: inputLocation, landmarks: [], date: inputDate, notes: inputNotes };
 
+        //Push additional landmarks to be displayed.
         $(".new-landmark").each(function() {
             var inputLandmark = $(this).val();
             newPlace.landmarks.push(inputLandmark);
         });
 
-
+        //Add to list of places below form
         $("ul#places").append("<li><span class='place'>" + newPlace.placeName + "</span></p>");
 
         $(".place").last().click(function() {
             $("#show-place").show();
 
+            //Display properties of place object on right side of page
             $("#show-place h2").text(newPlace.placeName);
             $(".location").text(newPlace.placeLocation);
             $(".landmarks").text(newPlace.landmarks.join(", "));
