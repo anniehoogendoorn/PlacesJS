6a815b79c4a5d1d303f22a8cce60e09fab9d9930
Kyle Pratuch & Annie Hoogendoorn <student@epicodus.com>
2015-09-14 15:43:13 -0700

    add ability to add multiple landmarks

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,48 @@
 $(document).ready(function(event) {
-    event.preventDefault();
+    $("#add-landmark").click(function() {
+        $("#new-landmarks").append(
+                                    '<div class="form-group">' +
+                                        '<label for="new-landmarks">Landmark</label>' +
+                                        '<input type="text" class="form-control new-landmark">' +
+                                        '</div>');
+    });
+
+
 
     $("form#new-place").submit(function(event) {
+        event.preventDefault();
 
         var inputPlaceName = $("input#new-place-name").val();
         var inputLocation = $("input#new-location").val();
-        var inputLandmarks = $("input#new-landmarks").val();
         var inputDate = $("input#new-date").val();
-        var inputNotes = $("input#new-notes").val();
+        var inputNotes = $("textarea#new-notes").val();
 
-        var newPlace = { placeName: inputPlaceName, placeLocation: inputLocation, landmarks: inputLandmarks date: inputDate, notes: inputNotes };
+        var newPlace = { placeName: inputPlaceName, placeLocation: inputLocation, landmarks: [], date: inputDate, notes: inputNotes };
+
+        $(".new-landmark").each(function() {
+            var inputLandmark = $(this).val();
+            newPlace.landmarks.push(inputLandmark);
+            console.log(newPlace.landmarks);
+        });
+
+
+        $("ul#places").append("<li><span class='place'>" + newPlace.placeName + "</span></p>");
+
+        $(".place").last().click(function() {
+            $("#show-place").show();
+
+            $("#show-place h2").text(newPlace.placeName);
+            $(".location").text(newPlace.placeLocation);
+            $(".landmarks").text(newPlace.landmarks.join(", "));
+            $(".date").text(newPlace.date);
+            $(".notes").text(newPlace.notes);
+
+            $("input#new-place-name").val("");
+            $("input#new-location").val("");
+            $("input#new-landmark").val("");
+            $("input#new-date").val("");
+            $("textarea#new-notes").val("");
+        });
     });
-
-    $("ul#places").append("<li><span class='place'>" + newPlace.placeName + "</span></p>");
-
-    $(".place").last().click(function() {
-        $("#show-place").show();
-
-        $("#show-place h2").text(newPlace.placeName);
-        $(".location").text(newPlace.placeLocation);
-        $(".landmarks").text(newPlace.landmarks);
-        $(".date").text(newPlace.date);
-        $(".notes").text(newPlace.notes);
-
-        $("input#new-place-name").val("");
-        $("input#new-location").val("");
-        $("input#new-landmarks").val("");
-        $("input#new-date").val("");
-        $("input#new-notes").val("");
-    });
+        // debugger;
 });
